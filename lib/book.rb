0c7fedefb223d03254921feaefff32254b34f4b8
Jacob Viti <thevitij64@gmail.com>
2020-07-20 22:25:49 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -4,5 +4,25 @@ def initialize(title)
   @title = title
 end
 
+def title
+end
+  @title
+def
+
+def author=(author)
+end
+  @author = author
+def
+
+def page_count=(page_count)
+end
+    @page_count = page_count
+def
+
+def genre=(genre)
+end
+    @genre = genre
+def
+
 
 end
